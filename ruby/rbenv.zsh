bc47a907b8c96f5a7f006ee4795752746073f0af
Mattias Niklewski <mattias@niklewski.com>
2012-09-17 23:01:38 +0200

    Revert "Oh my god! They killed ruby!"
    This reverts commit f0a539e6eecc063e9279122271ce54ce3909abae.

---
+++ ruby/rbenv.zsh
@@ -0,0 +1,23 @@
+if [[ -d $HOME/.rbenv ]]; then
+    # per-user installation of rbenv
+    export PATH="$HOME/.rbenv/bin:$PATH"
+
+    # expansion of: eval "$(rbenv init -)"
+    export PATH="/home/mattias/.rbenv/shims:${PATH}"
+    source "/home/mattias/.rbenv/libexec/../completions/rbenv.zsh"
+    # rehashing is slooow so disable it for now
+    #rbenv rehash 2>/dev/null
+    rbenv() {
+      local command="$1"
+      if [ "$#" -gt 0 ]; then
+        shift
+      fi
+
+      case "$command" in
+      shell)
+        eval `rbenv "sh-$command" "$@"`;;
+      *)
+        command rbenv "$command" "$@";;
+      esac
+    }
+fi
