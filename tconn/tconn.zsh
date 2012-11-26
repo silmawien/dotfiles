b79f913347c5dcbb3f79a9eea1885c7a771d71a8
Mattias Niklewski <mattias.niklewski@sonymobile.com>
2012-11-26 13:25:40 +0100

    Provide 32-bit tconn to avoid LD errors

--- tconn/tconn.zsh
+++ tconn/tconn.zsh
@@ -1,4 +1,5 @@
 if [[ -d $HOME/.tconn ]]; then
-    export LD_PRELOAD="$HOME/.tconn/tconn.so:$LD_PRELOAD"
+    export LD_LIBRARY_PATH=~/.tconn/'$LIB'
+    export LD_PRELOAD=tconn.so:$LD_PRELOAD
 fi
 
