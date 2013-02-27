599b8029f74bd66d5fc4f5a19492e9f003351ac7
Mattias Niklewski <mattias.niklewski@sonymobile.com>
2013-02-27 17:03:05 +0100

    Share history. Git push alias for work.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,12 @@
+function gpp() {
+    repo_path=$(git config --get remote.origin.url | sed 's/^.*review.sonyericsson.net//')
+    if [[ -n "$repo_path" ]]; then
+        git push ssh://mattias.niklewski@review.sonyericsson.net:29418$repo_path $@
+    else
+        echo "Could not parse remote.origin.url. Not a git repo?"
+    fi
+}
+
 alias gs="git status -sb"
 alias gd="git diff"
 alias gc="git commit"
