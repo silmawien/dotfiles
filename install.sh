0af5ff6f731042ed2a0965f87fac507d1c3184ef
Mattias Niklewski <mattias@niklewski.com>
2013-06-01 12:26:12 +0200

    Combine dotfiles and dotvim

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/zsh
 foreach target (~/.dotfiles/*.symlink)
-  base=`basename ${target} .symlink`
-  echo "Installing ${target} at ~/.${base}"
-  ln -s $target ~/.${base}
+  dst=$HOME/.`basename ${target} .symlink`
+  if [[ -a $dst || -h $dst ]]; then
+    echo Skipping `ls -ld $dst | awk '{print $9, $10, $11}'`
+  else
+    echo "Adding $dst -> $target"
+    ln -s $target $dst
+  fi
 end
