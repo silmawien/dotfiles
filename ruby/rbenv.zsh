f68f4ee2d212b8846f5d16a63a0eb8c7c754a0cf
Mattias Niklewski <mattias@niklewski.com>
2013-06-15 17:47:03 +0200

    rbenv is faster now, no need for workaround

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,22 +1,6 @@
 if [[ -d $HOME/.rbenv ]]; then
     # per-user installation of rbenv
     export PATH="$HOME/.rbenv/bin:$PATH"
-    
-    # expansion of: eval "$(rbenv init -)"
-    export PATH="/home/mattias/.rbenv/shims:${PATH}"
-    source "/home/mattias/.rbenv/libexec/../completions/rbenv.zsh"
-    rbenv rehash 2>/dev/null
-    rbenv() {
-      local command="$1"
-      if [ "$#" -gt 0 ]; then
-        shift
-      fi
 
-      case "$command" in
-      shell)
-        eval `rbenv "sh-$command" "$@"`;;
-      *)
-        command rbenv "$command" "$@";;
-      esac
-    }
+    eval "$(rbenv init -)"
 fi
