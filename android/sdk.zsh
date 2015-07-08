ed4092319b52576a028cc56eeb5efa51bcde8e74
Mattias Niklewski <mattias.niklewski@sonymobile.com>
2015-07-08 15:03:33 +0200

    nvm + misc

--- android/sdk.zsh
+++ android/sdk.zsh
@@ -1,5 +1,6 @@
 if [ -e ~/sandbox/current-sdk ]; then
     export ANDROIDSDK_HOME=~/sandbox/current-sdk
+    export ANDROID_HOME=$ANDROIDSDK_HOME
     PATH=${ANDROIDSDK_HOME}/platform-tools:$PATH
     PATH=${ANDROIDSDK_HOME}/tools:$PATH
 fi
--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,12 +1,3 @@
-function gpp() {
-    repo_path=$(git config --get remote.origin.url | sed -E 's/^.*review.sonyericsson.net(:\d+)?//')
-    if [[ -n "$repo_path" ]]; then
-        git push ssh://mattias.niklewski@review.sonyericsson.net:29418$repo_path $@
-    else
-        echo "Could not parse remote.origin.url. Not a git repo?"
-    fi
-}
-
 alias gs="git status -sb"
 alias gd="git diff"
 alias gc="git commit"
