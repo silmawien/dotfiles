1cebe69e03ffeef05323b2a581827fec6efe32a6
Mattias Niklewski <mattias.niklewski@sony.com>
2021-02-04 16:31:34 +0100

    Retire tconn
    Hopefully anyway.

--- android/adb.zsh
+++ android/adb.zsh
@@ -2,9 +2,9 @@ function input() {
     first="t"
     foreach w in ${=@}
         if [[ -z "$first" ]]; then
-            adb -d shell input keyevent 62 # space
+            adb shell input keyevent 62 # space
         fi
         unset first
-        adb -d shell input text $w
+        adb shell input text $w
     end
 }
